22747947c4a031a32958b5b326623fb3ee0647d8
azmalent <dmitrysjestov@niuitmo.ru>
2021-11-07 13:28:36 +0300

    Lab1 final tweaks

--- 1/algorithms/lab1/main.cpp
+++ 1/algorithms/lab1/main.cpp
@@ -13,6 +13,8 @@ HammurapiGame createGame()
         cout << "\nFound a save file. Would you like to continue your previous game? (y/n) ";
         char c;
         cin >> c;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
         if (c == 'y') 
         {
             HammurapiGame game = HammurapiGame::load(fin);
